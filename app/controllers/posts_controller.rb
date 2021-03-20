abad8f0892726f2a455316138e50b035e358c95c
keento0809 <handball0321@gmail.com>
2021-03-20 14:32:11 +0900

    change scss

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,30 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new()
+  end
+
+  def create
+    if user_signed_in?
+      @post = Post.new(new_post_params)
+      if @post.save!
+        flash[:notice] = "New post created!"
+      else
+        flash[:danger] = "Failed to create new post."
+        redirect_to new_post_path
+      end
+
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(params[:id])
+    @post.destroy
+    redirect_to root_url, notice: "Post deleted."
+  end
+
+  private
+
+  def new_post_params
+    params.require(:post).permit(:user_id, :cafename, :img, :comment)
   end
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,6 +3,7 @@ class TopController < ApplicationController
   
   def index
     @user = User.find_by(id: current_user.id);
+    @posts = Post.all
   end
 
   def show
